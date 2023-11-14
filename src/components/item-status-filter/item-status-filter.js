afad3786d5ebc2857c28cad1836a78ed5991089f
rgphpacademy <rolandas.gylys@gmail.com>
2023-11-14 16:30:54 +0200

    second commit

---
+++ src/components/item-status-filter/item-status-filter.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+// class ItemStatusFilter extends Component {
+//   state = {  }
+//   render() {
+//     return (  );
+//   }
+// }
+
+// export default ItemStatusFilter;
+
+const filterButtons = [
+  { name: "all", label: "All" },
+  { name: "active", label: "Active" },
+  { name: "done", label: "Done" }
+];
+
+const ItemStatusFilter = ({ filter, onFilterChange = () => {} }) => {
+  const buttons = filterButtons.map(({ name, label }) => {
+    const isActive = name === filter;
+    const classNames =
+      "btn " + (isActive ? "btn-info" : "btn-outline-secondary");
+
+    return (
+      <button
+        key={name}
+        type="button"
+        onClick={() => onFilterChange(name)}
+        className={classNames}
+      >
+        {label}
+      </button>
+    );
+  });
+
+  return <div className="btn-group">{buttons}</div>;
+};
+
+export default ItemStatusFilter;
